270cbc29f68999abdbf0f98a1a0e20c8f5dd14bf
Matheus Varela <matheus_s_varela@hotmail.com>
2023-06-24 17:36:53 -0300

    Creating a model card to page evento

--- API/criaCliente.js
+++ API/criaCliente.js
@@ -1,30 +1,30 @@
 import { conectaApi } from "./conectaApi.js";
+const formulario = document.querySelector("[data-formulario]");
 
 async function cadastrarCliente() {
-    preventDefault();
-    let nome = document.querySelector("[data-nome]").value;
-    let nascimento = document.querySelector("[data-nascimento]").value;
-    let cpf = document.querySelector("[data-cpf]").value;
-    let contato = document.querySelector("[data-contato]").value;
-    let cep = document.querySelector("[data-cep]").value;
-    let estado = document.querySelector("[data-estado]").value;
-    let cidade = document.querySelector("[data-cidade]").value;
-    let endereco = document.querySelector("[data-endereco]").value;
-    let bairro = document.querySelector("[data-bairro]").value;
-    let numero = document.querySelector("[data-numero]").value;
-    let complemento = document.querySelector("[data-complemento]").value;
-    let email = document.querySelector("[data-email]").value;
-    let senha = document.querySelector("[data-senha]").value;
+    // preventDefault();
+    const nome = document.querySelector("[data-nome]").value;
+    const nascimento = document.querySelector("[data-nascimento]").value;
+    const cpf = document.querySelector("[data-cpf]").value;
+    const contato = document.querySelector("[data-contato]").value;
+    const cep = document.querySelector("[data-cep]").value;
+    const estado = document.querySelector("[data-estado]").value;
+    const cidade = document.querySelector("[data-cidade]").value;
+    const endereco = document.querySelector("[data-endereco]").value;
+    const bairro = document.querySelector("[data-bairro]").value;
+    const numero = document.querySelector("[data-numero]").value;
+    const complemento = document.querySelector("[data-complemento]").value;
+    const email = document.querySelector("[data-email]").value;
+    const senha = document.querySelector("[data-senha]").value;
     
     try{    
         await conectaApi.criaCliente(nome, nascimento, cpf, contato, cep, estado, 
             cidade, endereco, bairro, numero, complemento, email, senha);
         
-            window.location.href = "../modalCadastroSucesso.html";
+            window.location.href = "../index.html";
     }catch(e){
         alert(e);       
     }
-
-
-
 }
+
+formulario.addEventListener("submit", evento => cadastrarCliente(evento));
