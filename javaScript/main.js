fe541e70e70235a533d74315158300863788f841
Matheus Varela <matheus_s_varela@hotmail.com>
2023-06-29 01:46:48 -0300

    Merge e  correções/melhorias

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,15 +1,17 @@
+let usuarioAutenticado = false;
+
 function logar() {
 
     var email = document.getElementById('floatingInput').value;
     var senha = document.getElementById('floatingPassword').value;
 
     if (email == "admin@sca" && senha == '123') {
-        alert('sucesso');
+        usuarioAutenticado = true;
         location.href = "home.html";
     } else {
         alert('Usuario ou senha incorretos');
     }
-
+    return usuarioAutenticado;
 }
 
 // Função controle quantidade de ingresso--------------------------------------------------------
