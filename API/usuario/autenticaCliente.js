11869b4efb8ecbef91dc8c57996147469f39e942
Matheus Varela <matheus_s_varela@hotmail.com>
2023-07-05 19:02:58 -0300

    apresentaçáo

--- API/usuario/autenticaCliente.js
+++ API/usuario/autenticaCliente.js
@@ -1,45 +1,22 @@
-import { conectaApi } from "../conectaApi.js";
-
-let Senha = document.getElementById('floatingPassword').value;
-
-function logar(email, senha) {
-
+function logar() {
+    
+    let email = document.getElementById('floatingInput').value;
+    let senha = document.getElementById('floatingPassword').value;
     let usuarioAutenticado = false;
     
-    if (Email == "admin@admin.com" && Senha == 'Admin.321') {
+    if (email == "admin@admin.com" && senha == 'Admin.321') {
+
         usuarioAutenticado = true;
-        preventDefault();
+        location.href = "../../src/cadastro-evento.html";
         alert("Seja bem-vindo ADM!");
 
-        location.href = "../../src/cadastro-evento.html";
-    } else if (Email == "user@user.com" && Senha == "123456") {
+    } else if (email == "user@user.com" && senha == "123456") {
+
         usuarioAutenticado = true;
-        alert("Seja bem-vindo!")
-        
         location.href = "../../index.html";
+        alert("Seja bem-vindo!")
+
     } else {
         alert('Usuário ou senha incorretos!');
-        preventDefault();
     }
 }
-
-async function listarUsuarios(){
-    try{
-        let Email = document.getElementById('floatingInput').value; 
-
-        const listaUsuarioApi = await conectaApi.listarUsuarios();
-        let listaFiltrada = listaUsuarioApi.filter(v => v.email.includes(`${Email}`));
-
-        listaFiltrada.forEach(element => ()=>{
-            logar(element.email, element.senha)
-        });
-
-    }catch (error){
-        alert(`Não foi possível carregar a lista de usuários</h2> ${error}`);
-    }
-}
-
-
-
-const login = document.getElementById("button__login");
-// login.addEventListener("click", evento => listarUsuarios());
